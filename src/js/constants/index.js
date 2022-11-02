b388b872618847d29ff752b759ae57b57d217d6e
alex-lenk <alexlenk.ru@gmail.com>
2022-11-02 16:19:56 +0300

    Created Tab component

--- src/js/constants/index.js
+++ src/js/constants/index.js
@@ -2,5 +2,6 @@ export const body = document.body
 export const jsToggleMenu = document.querySelector('.js-toggle-nav')
 export const jsHomeEl = document.querySelector('.js-home')
 export const homeContentBtn = document.querySelector('.js-home__content-btn')
+export const tabNav = document.querySelector('.js-tab__nav')
 
 export const widthScreen = document.documentElement.clientWidth
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import App from './core/App'
 import ToggleMenu from './components/toggleMenu.components'
 import HomePage from './components/homePage'
+import Tab from './components/tab'
 
 const app = new App([
   ToggleMenu,
-  HomePage
+  HomePage,
+  Tab
 ])
 
 app.run()
